29a85e35c5ad245093f0d1401c274b6f6faad0f8
Shane Lin <ssl2ab@virginia.edu>
2018-10-15 15:29:49 -0400

    add usage column

--- ical_counter.py
+++ ical_counter.py
@@ -5,13 +5,14 @@ import csv
 
 PATH = "."
 
-filenames = os.listdir(PATH)
+filenames = [f for f in os.listdir('.') if os.path.isfile(f)]
 hours = defaultdict(int)
+uses = defaultdict(int)
 for fname in filenames:
     with open(PATH+"/"+fname) as icsfile:
         if fname[-3:] != "ics":
             continue
-        print(fname)
+        print("Reading: ",fname)
         line = icsfile.readline()
         while line:
             if line[0:8] == "DTSTART:":
@@ -21,11 +22,12 @@ for fname in filenames:
                     end = parse(line[6:])
                     delta = end-start
                     hours[start.strftime("%Y-%m")] += delta.seconds / 3600
+                    uses[start.strftime("%Y-%m")] += 1
             line = icsfile.readline()
 
 with open('ical_counter.csv', 'w') as outfile:    
-    w = csv.DictWriter(outfile, ["month","hours"])
+    w = csv.DictWriter(outfile, ["month","uses","hours"])
     w.writeheader()
     for month in sorted(list(hours.keys())):
-        w.writerow({'month':month,'hours':round(hours[month],2)})
+        w.writerow({'month': month, 'uses': uses[month],'hours': round(hours[month], 2)})
     
